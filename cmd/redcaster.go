3ca9252c5932b32c67f9ace3a5c9fd3936dabceb
Reda Baydoun <reda.baydoun@gmail.com>
2024-08-04 19:22:53 -0400

    feat: more WIP -- draw floor, ceiling, learn OpenGL coord system

--- cmd/redcaster.go
+++ cmd/redcaster.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	rp "github.com/rebay1982/redpix"
+	"time"
 	//"fmt"
 )
 
@@ -15,6 +16,7 @@ const (
 
 type Game struct {
 	playerX, payerY float64
+	clear bool
 }
 
 type Renderer struct {
@@ -42,8 +44,9 @@ var gameMap = [16][16]int{
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
 }
 
-func (g Game) update() {
-
+func (g *Game) update() {
+	time.Sleep(1 * time.Second)
+	g.clear = !g.clear
 }
 
 func NewRenderer(game *Game) *Renderer{
@@ -55,29 +58,63 @@ func NewRenderer(game *Game) *Renderer{
 	return r
 }
 
-func (r Renderer) drawVertical(x int) {
-	height := FB_HEIGHT / 6
-	startHeight := (FB_HEIGHT - height) >> 1
+func (r Renderer) drawCeiling() {
+	height := FB_HEIGHT >> 1
+	for x := 0; x < FB_WIDTH; x++ {
+		for y := FB_HEIGHT -1; y >= height; y-- {
+			colorIndex := (x + y * FB_WIDTH) * 4
+			r.frameBuffer[colorIndex + 2] = 0xFF	// Blue skies component
+			r.frameBuffer[colorIndex + 3] = 0xFF  // Alpha
+		}
+	}
+}
 
-	//fmt.Printf("h: %d, sh: %d", height, startHeight)
+func (r Renderer) drawFloor() {
+	height := FB_HEIGHT >> 1
+	for x := 0; x < FB_WIDTH; x++ {
+		for y := height; y >= 0; y-- {
+			colorIndex := (x + y * FB_WIDTH) * 4
+			r.frameBuffer[colorIndex + 1] = 0x7F	// Green grass component
+			r.frameBuffer[colorIndex + 3] = 0xFF  // Alpha
+		}
+	}
+}
+
+func (r Renderer) drawVertical(x int) {
+	height := FB_HEIGHT / 2
+	startHeight := (FB_HEIGHT - height) >> 1
 
 	for y := startHeight; y < (startHeight + height); y++ {
 		colorIndex := (x + y * FB_WIDTH) * 4
 		r.frameBuffer[colorIndex + 1] = 0xFF	// Green component
+		r.frameBuffer[colorIndex + 2] = 0xFF	// Blue component
 		r.frameBuffer[colorIndex + 3] = 0xFF  // Alpha
 	}
 }
 
+func (r *Renderer) clearFrameBuffer() {
+	r.frameBuffer[0] = 0x00
+	for i := 1; i < len(r.frameBuffer); i = i << 1 {
+		copy(r.frameBuffer[i:], r.frameBuffer[:i])
+	}
+}
+
 func (r Renderer) draw() []uint8 {
+	r.clearFrameBuffer()
+
+	r.drawCeiling()
+	r.drawFloor()
+	
+	// Draw walls
 	for x := 0; x < FB_WIDTH; x++ {
 		r.drawVertical(x)
 	}
-
+	
 	return r.frameBuffer
 }
 
 func main() {
-	game := Game{0, 0}
+	game := Game{0, 0, false}
 	renderer := NewRenderer(&game)
 
 	config := rp.WindowConfig{
@@ -88,6 +125,8 @@ func main() {
 		VSync: true,
 	}
 
+	go func(){ for {game.update()}}() 
+
 	rp.Init(config)
-	rp.Run(game.update, renderer.draw)
+	rp.Run(nil, renderer.draw)
 }
